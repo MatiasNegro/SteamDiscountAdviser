f45da1830448f4710dfc654580a42a14677241b2
MatiasNegro <67411732+MatiasNegro@users.noreply.github.com>
2022-09-30 16:58:39 +0200

    Changed the logo, Respaced left columnn

--- code/steam_discount_adviser/lib/gameListWidgetBuilder.dart
+++ code/steam_discount_adviser/lib/gameListWidgetBuilder.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:steam_discount_adviser/requests.dart';
@@ -36,23 +38,55 @@ class _SelectedGamesState extends State<SelectedGames> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             //Sized boxes are there just make space
-            const SizedBox(
-              height: 10,
-            ),
+
             //Display the logo of the application
             Center(
-              child: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage('./assets/images/logo.png'),
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        gradient: RadialGradient(colors: [
+                          Colors.blueGrey,
+                          Color.fromARGB(255, 2, 18, 45) as Color,
+                          Colors.blueGrey[900] as Color,
+                        ], radius: 2.0),
+                        shape: BoxShape.circle,
+                        border: Border.all(width: 2.0)),
+                    height: 200,
+                    width: 200,
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(25.0, 0.0, 0.0, 0.0),
+                        child: Text(
+                          "Steam Discount Adviser",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontFamily: 'RobotoMono',
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 10),
+            //const SizedBox(height: 10),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blueGrey,
+                ),
+                child: Text(
+                  "Selected games",
+                  style: TextStyle(
+                    fontFamily: "RobotoMono",
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
             Expanded(
                 //Creation of the list.
                 child: FutureBuilder(
